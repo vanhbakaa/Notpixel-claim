199b8d16d46624fd2d5ca7f867f8948a2a8a0dc2
Vanhbaka <vanhbaka2k7@gmail.com>
2024-11-03 10:57:02 +0700

    Update headers.py

--- bot/core/headers.py
+++ bot/core/headers.py
@@ -8,9 +8,8 @@ headers = {
     'Sec-Fetch-Dest': 'empty',
     'Sec-Fetch-Mode': 'cors',
     'Sec-Fetch-Site': 'same-site',
-    'x-requested-with': "org.telegram.messenger",
-    'sec-ch-ua': '"Chromium";v="130", "Android WebView";v="130", "Not?A_Brand";v="99"',
-    'sec-ch-ua-mobile': "?1",
+    'Sec-Ch-Ua': '"Chromium";v="130", "Android WebView";v="130", "Not?A_Brand";v="99"',
+    'Sec-Ch-Aa-Mobile': "?1",
     'Sec-Ch-Ua-platform': '"Android"',
     'User-Agent': 'Mozilla/5.0 (Linux; Android 14) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.6422.165 Mobile Safari/537.36'
 }
