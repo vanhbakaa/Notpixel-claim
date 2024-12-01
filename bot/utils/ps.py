1ac7dc8a746b43a214317fe1a8a4f97d00ee2cad
Vanhbaka <vanhbaka2k7@gmail.com>
2024-12-01 17:17:26 +0700

    Create ps.py

---
+++ bot/utils/ps.py
@@ -0,0 +1,123 @@
+import sys
+import requests
+import re
+
+from bot.config import settings
+from bot.utils import logger
+
+baseUrl = "https://notpx.app/api/v1/"
+
+apis = [
+    "/users/me",
+    "/users/stats",
+    "/image/template/my",
+    "/mining/status",
+    "/image/template/subscribe/",
+    "/mining/claim",
+    "/mining/boost/check/",
+    "/mining/task/check/"
+]
+ls_pattern = re.compile(r'\b[a-zA-Z]+\s*=\s*["\'](https?://[^"\']+)["\']')
+e_get_pattern = re.compile(r'[a-zA-Z]\.get\(\s*["\']([^"\']+)["\']|\(\s*`([^`]+)`\s*\)')
+e_put_pattern = re.compile(r'[a-zA-Z]\.put\(\s*["\']([^"\']+)["\']|\(\s*`([^`]+)`\s*\)')
+
+
+version = "1.0.3"
+def clean_url(url):
+    url = url.split('?')[0]
+    url = re.sub(r'\$\{.*?\}', '', url)
+    url = re.sub(r'//+', '/', url)
+    return url
+
+def get_main_js_format(base_url):
+    try:
+        response = requests.get(base_url)
+        response.raise_for_status()  # Raises an HTTPError for bad responses
+        content = response.text
+        matches = re.findall(r'src="(/.*?/index.*?\.js)"', content)
+        if matches:
+            # Return all matches, sorted by length (assuming longer is more specific)
+            return sorted(set(matches), key=len, reverse=True)
+        else:
+            return None
+    except requests.RequestException as e:
+        logger.warning(f"Error fetching the base URL: {e}")
+        return None
+
+def get_base_api(url):
+    try:
+        # logger.info("Checking for changes in api...")
+        response = requests.get(url)
+        response.raise_for_status()
+        content = response.text
+        match = ls_pattern.findall(content)
+        e_get_urls = e_get_pattern.findall(content)
+        e_put_urls = e_put_pattern.findall(content)
+
+        if e_get_urls is None:
+            return None
+
+        urls = [url[0] if url[0] else url[1] for url in e_get_urls]
+        urls_put = [url[0] if url[0] else url[1] for url in e_put_urls]
+        clean_urls = [clean_url(url) for url in urls] + [clean_url(url) for url in urls_put]
+
+        for url in apis:
+            if url not in clean_urls:
+                logger.warning(f"<yellow>api {url} changed!</yellow>")
+                return None
+
+        if match:
+            # print(match)
+            return match
+        else:
+            logger.info("Could not find 'api' in the content.")
+            return None
+
+    except requests.RequestException as e:
+        logger.warning(f"Error fetching the JS file: {e}")
+        return None
+
+
+def check_base_url():
+    base_url = "https://app.notpx.app/"
+    main_js_formats = get_main_js_format(base_url)
+
+    if main_js_formats:
+        if settings.ADVANCED_ANTI_DETECTION:
+            r = requests.get("https://raw.githubusercontent.com/vanhbakaa/nothing/refs/heads/main/px")
+            js_ver = r.text.strip()
+            version_c = js_ver.split(",")[1]
+            if version != version_c:
+                logger.warning(f"<red>Detected danger change must update the bot for safety!</red>")
+                sys.exit()
+            js_ver = js_ver.split(",")[0]
+            for js in main_js_formats:
+                if js_ver in js:
+                    logger.success(f"<green>No change in js file: {js_ver}</green>")
+                    return True
+            return False
+        else:
+            for format in main_js_formats:
+                # logger.info(f"Trying format: {format}")
+                full_url = f"https://app.notpx.app{format}"
+                result = get_base_api(full_url)
+                # print(f"{result} | {baseUrl}")
+                if result is None:
+                    return False
+
+                if baseUrl in result:
+                    logger.success("<green>Bot is safe to run!</green>")
+                    return True
+                return False
+            else:
+                logger.warning("Could not find 'baseURL' in any of the JS files.")
+                return False
+    else:
+        logger.info("Could not find any main.js format. Dumping page content for inspection:")
+        try:
+            response = requests.get(base_url)
+            print(response.text[:1000])  # Print first 1000 characters of the page
+            return False
+        except requests.RequestException as e:
+            logger.warning(f"Error fetching the base URL for content dump: {e}")
+            return False
